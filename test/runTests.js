ebb094779229a0f5d207fd1e7330649ec70bcfda
veilance <christopher.tam@alumni.ubc.ca>
2018-10-17 16:06:49 -0700

    cleaned up test cases

--- test/runTests.js
+++ test/runTests.js
@@ -6,20 +6,22 @@ const ReadText = require(path.resolve("./readText"));
 const SummaryReport = require(path.resolve("./summaryReport"));
 
 describe("ReadText Stream Test", function() {
-  describe("ReadText Stream number of lines and total length in bytes", function() {
+  describe("Falsy value is passed into ReadTextStream", function() {
     const ReadTextStream = new ReadText();
     it("should throw error message when empty chunk passed to stream", function() {
       assert.throws(
-        () => {ReadTextStream.write()},
+        () => {
+          ReadTextStream.write();
+        },
         /Empty chunk passed/,
         "incorrect error thrown on empty chunk"
-      )
+      );
     });
   });
 
-  describe("ReadText Stream number of lines and total length in bytes", function() {
+  describe("ReadText Stream is passed text line Hii", function() {
     const ReadTextStream = new ReadText();
-    it("should return 1 line and 3 bytes when text is Hii", function() {
+    it("should return 1 line read and 3 bytes when text is Hii", function() {
       ReadTextStream.write("Hii");
       assert.equal(
         ReadTextStream.obj.number_of_lines,
@@ -34,9 +36,9 @@ describe("ReadText Stream Test", function() {
     });
   });
 
-  describe("ReadText Stream number of lines and total length in bytes", function() {
+  describe("ReadText Stream is passed Hii\\nHello hello", function() {
     const ReadTextStream = new ReadText();
-    it("should return 2 lines and 15 bytes when text is Hii\\nHello hello", function() {
+    it("should return 2 lines read and 15 bytes when text is Hii\\nHello hello", function() {
       ReadTextStream.write("Hii\nHello hello");
       assert.equal(
         ReadTextStream.obj.number_of_lines,
@@ -51,9 +53,9 @@ describe("ReadText Stream Test", function() {
     });
   });
 
-  describe("ReadText Stream number of lines and total length in bytes in Chinese", function() {
+  describe("ReadText Stream is passed chinese text ⺄⺅⺇⺋⺋", function() {
     const ReadTextStream = new ReadText();
-    it("should return 1 lines and 15 bytes when text is ⺄⺅⺇⺋⺋", function() {
+    it("should return 1 line read and 15 bytes when text is ⺄⺅⺇⺋⺋", function() {
       ReadTextStream.write("⺄⺅⺇⺋⺋");
       assert.equal(
         ReadTextStream.obj.number_of_lines,
@@ -70,8 +72,8 @@ describe("ReadText Stream Test", function() {
 });
 
 describe("SummaryReport Stream Test", function() {
-  // base case input is empty 
-  describe("SummaryReport Stream Read Lines and Growth Rate", function() {
+  // base case input is empty
+  describe("SummaryReport is passed an obj with base case of 0 for all params", function() {
     const obj = {
       total_length_in_bytes: 0,
       number_of_lines: 0,
@@ -80,7 +82,7 @@ describe("SummaryReport Stream Test", function() {
     it("should return 0 lines and 0 bytes/s when the input is empty", function() {
       const SummaryReportStream = new SummaryReport();
       SummaryReportStream.write(obj);
-      const msg = SummaryReportStream.read().toString()
+      const msg = SummaryReportStream.read().toString();
       assert.equal(
         msg,
         `0 lines read and growth of file is 0.00 bytes/s\n`,
@@ -90,50 +92,64 @@ describe("SummaryReport Stream Test", function() {
   });
 
   // test with 1 line
-  describe("SummaryReport Stream Read Lines and Growth Rate", function() {
+  describe("SummaryReport Stream is passed an obj with total_length_in_bytes: 50, number_of_lines: 1 and elapsed_time: 150", function() {
     const obj = {
       total_length_in_bytes: 50,
       number_of_lines: 1,
       elapsed_time: 150
     };
-    it(`should return ${obj.number_of_lines} lines read and growth of file is ${(obj.total_length_in_bytes/(obj.elapsed_time/1000)).toFixed(2)} bytes/s when the input is empty`, function() {
+    it(`should return ${
+      obj.number_of_lines
+    } lines read and growth of file is ${(
+      obj.total_length_in_bytes /
+      (obj.elapsed_time / 1000)
+    ).toFixed(2)} bytes/s`, function() {
       const SummaryReportStream = new SummaryReport();
       SummaryReportStream.write(obj);
-      const msg = SummaryReportStream.read().toString()
+      const msg = SummaryReportStream.read().toString();
       assert.equal(
         msg,
-        `${obj.number_of_lines} lines read and growth of file is ${(obj.total_length_in_bytes/(obj.elapsed_time/1000)).toFixed(2)} bytes/s\n`,
-        `lines or growth of file incorrect`,
+        `${obj.number_of_lines} lines read and growth of file is ${(
+          obj.total_length_in_bytes /
+          (obj.elapsed_time / 1000)
+        ).toFixed(2)} bytes/s\n`,
+        `lines or growth of file incorrect`
       );
     });
   });
 
   // test 10 lines
-  describe("SummaryReport Stream Read Lines and Growth Rate", function() {
+  describe("SummaryReport Stream is passed an obj with total_length_in_bytes: 4000, number_of_lines: 10 and elapsed_time: 0", function() {
     const obj = {
       total_length_in_bytes: 4000,
       number_of_lines: 10,
       elapsed_time: 0
     };
-    it(`should return 10 lines and ${(obj.total_length_in_bytes/(obj.elapsed_time/1000)).toFixed(2)} bytes/s when the input is empty`, function() {
+    it(`should return 10 lines and ${(
+      obj.total_length_in_bytes /
+      (obj.elapsed_time / 1000)
+    ).toFixed(2)} bytes/s when the input is empty`, function() {
       const SummaryReportStream = new SummaryReport();
       SummaryReportStream.write(obj);
-      const msg = SummaryReportStream.read().toString()
+      const msg = SummaryReportStream.read().toString();
       assert.equal(
         msg,
-        `${obj.number_of_lines} lines read and growth of file is ${(obj.total_length_in_bytes/(1)).toFixed(2)} bytes/s\n`,
+        `${obj.number_of_lines} lines read and growth of file is ${(
+          obj.total_length_in_bytes / 1
+        ).toFixed(2)} bytes/s\n`,
         "lines or growth of file incorrect"
       );
     });
   });
 
-// Test when the object is falsy
+  // Test when the object is falsy
   describe("Test when the falsy value passed to SummaryReportStream", function() {
-    it(`should return error message`, function() {
+    it(`should return error message: Empty chunk passed in SummaryReport Stream`, function() {
       const SummaryReportStream = new SummaryReport();
-      ;
       assert.throws(
-        () => {SummaryReportStream.write()},
+        () => {
+          SummaryReportStream.write();
+        },
         /Empty chunk passed in SummaryReport Stream/,
         "error message wasn't thrown on falsy object passed to stream"
       );
